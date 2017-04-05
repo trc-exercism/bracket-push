5a73980bdd2cbf042d4dd11495e79890032b169e
Devin Nathan-Turner <devin.nathan-turner@thinkresearch.com>
2017-04-05 13:07:27 -0400

    Almost there

--- bracket_push.exs
+++ bracket_push.exs
@@ -2,8 +2,22 @@ defmodule BracketPush do
   @doc """
   Checks that all the brackets and braces in the string are matched correctly, and nested correctly
   """
+  @brackets '{}[]()'
+  @bracket_dict %{'{' => '}', '[' => ']', '(' => ')'}
+
   @spec check_brackets(String.t) :: boolean
   def check_brackets(str) do
-
+    _match_bracket(to_charlist(str), [])
   end
+
+  defp _match_bracket([], bracket_list), do: Enum.empty?(bracket_list)
+  defp _match_bracket([h|t], bracket_list) when h in @brackets do
+      cond do
+        Map.has_key?(@bracket_dict, h) -> _match_bracket(t, h ++ bracket_list)
+        Enum.empty?(bracket_list) -> false
+        @bracket_dict[hd(bracket_list)] == h -> _match_bracket(t, tl(bracket_list))
+        true -> false
+      end
+  end
+  defp _match_bracket([h|t], bracket_list), do: _match_bracket(t, bracket_list)
 end
--- bracket_push_test.exs
+++ bracket_push_test.exs
@@ -8,57 +8,57 @@ ExUnit.configure exclude: :pending, trace: true
 defmodule BracketPushTest do
   use ExUnit.Case
 
-  # @tag :pending
+  # #@tag :pending
   test "empty string" do
     assert BracketPush.check_brackets("")
   end
 
-  @tag :pending
+  #@tag :pending
   test "appropriate bracketing in a set of brackets" do
     assert BracketPush.check_brackets("{}")
   end
 
-  @tag :pending
+  #@tag :pending
   test "unclosed brackets" do
     refute BracketPush.check_brackets("{{")
   end
 
-  @tag :pending
+  #@tag :pending
   test "more than one pair of brackets" do
     assert BracketPush.check_brackets("{}[]")
   end
 
-  @tag :pending
+  #@tag :pending
   test "brackets are out of order" do
     refute BracketPush.check_brackets("}{")
   end
 
-  @tag :pending
+  #@tag :pending
   test "nested brackets" do
     assert BracketPush.check_brackets("{[()]}")
   end
 
-  @tag :pending
+  #@tag :pending
   test "unbalanced nested brackets" do
     refute BracketPush.check_brackets("{[}]")
   end
 
-  @tag :pending
+  #@tag :pending
   test "bracket closure with deeper nesting" do
     refute BracketPush.check_brackets("{[)][]}")
   end
 
-  @tag :pending
+  #@tag :pending
   test "bracket closure in a long string of brackets" do
     assert BracketPush.check_brackets("{[]([()])}")
   end
 
-  @tag :pending
+  #@tag :pending
   test "should ignore non-bracket characters" do
     assert BracketPush.check_brackets("{hello[]([a()])b}c")
   end
 
-  @tag :pending
+  #@tag :pending
   test "string with newlines" do
     assert BracketPush.check_brackets("[]\n{()}\n[(({}))]\n")
   end
